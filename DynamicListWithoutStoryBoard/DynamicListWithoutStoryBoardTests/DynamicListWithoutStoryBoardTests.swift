728295153f331ebe7b755c39835ef0521ea2b45f
Raju <KARROTHU.APPALARAJU@mahindra.com>
2019-12-09 22:35:30 +0530

    test case for API call success state

--- DynamicListWithoutStoryBoard/DynamicListWithoutStoryBoardTests/DynamicListWithoutStoryBoardTests.swift
+++ DynamicListWithoutStoryBoard/DynamicListWithoutStoryBoardTests/DynamicListWithoutStoryBoardTests.swift
@@ -11,17 +11,37 @@ import XCTest
 
 class DynamicListWithoutStoryBoardTests: XCTestCase {
 
+    var viewModel: HomeViewModel!
     override func setUp() {
+        super.setUp()
+        viewModel = HomeViewModel()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
+        viewModel = nil
+        super.tearDown()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testSuccessGettingList() {
+        let promise = expectation(description: "downloaded successfully")
+        viewModel.getList { (state) in
+            switch state {
+            case .Success(_):
+                promise.fulfill()
+            default:
+                XCTFail("download failed")
+                break
+            }
+        }
+        
+        wait(for: [promise], timeout: 5)
     }
 
     func testPerformanceExample() {
